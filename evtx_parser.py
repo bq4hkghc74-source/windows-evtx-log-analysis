9698b2ccff2c67feebd6ea458163e8141f70fb02
bq4hkghc74-source <bq4hkghc74@privaterelay.appleid.com>
2026-03-31 03:36:37 +0100

    Delete evtx_parser.py

--- evtx_parser.py
+++
@@ -1,8 +0,0 @@
-from Evtx.Evtx import Evtx
-
-with Evtx("logs/Security.evtx") as log:
-    for record in log.records():
-        xml = record.xml()
-        if "4688" in xml:
-            print(xml)
-            break 
